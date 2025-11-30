e6f43b2b64e0bc5ad01d54c05f760a8d17379eba
Dennis Schmid <dennis.schmid99@gmail.com>
2025-11-30 18:44:17 +0100

    added class "lieferant" and corresponding method

---
+++ Lieferant.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Lieferant here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lieferant
+{
+    /**
+     * Standard-Konstruktor für die Klasse Lieferant.
+     */
+    public Lieferant() {
+        // Aktuell sind keine Initialisierungen notwendig.
+    }
+
+    /**
+     * Diese Methode wird aufgerufen, um eine Materialbestellung beim Lieferanten aufzugeben.
+     * In dieser Simulation wird der Vorgang durch eine Konsolenausgabe bestätigt.
+     */
+    public void bestellungAufgeben() {
+        // Hier würde in einem realen System die API des Lieferanten angesprochen werden.
+        System.out.println("LIEFERANT: Bestellung erhalten. Ware wird umgehend versendet.");
+    }
+}    
